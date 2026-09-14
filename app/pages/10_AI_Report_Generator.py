80c1b8f0ab10c99668464f8802c3de0c252cb7d7
rob <rpowell8519@gmail.com>
2026-09-14 21:58:47 +0100

    Mark report generator hotfix build

--- app/pages/10_AI_Report_Generator.py
+++ app/pages/10_AI_Report_Generator.py
@@ -77,7 +77,7 @@ from src.report_generator_readiness import (  # noqa: E402
 )
 
 
-BUILD_VERSION = "Accessible AI Report Generator v2.4"
+BUILD_VERSION = "Accessible AI Report Generator v2.4.1"
 REPORT_STATE_KEY = "accessible_ai_report_generator_result"
 AI_VISIBILITY_HANDOFF_KEY = "ai_visibility_report_handoff_target"
 AI_VISIBILITY_FORCE_PROMPTS_KEY = "ai_visibility_force_owner_prompts"
